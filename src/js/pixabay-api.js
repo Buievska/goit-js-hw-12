d8c798a6ab9215c9760daa22e23dadb96f850abd
Buievska <anna111239@gmail.com>
2025-07-24 21:30:25 +0200

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '51367924-123f738920e3f369a979532e9';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      image => `
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" />
+        </a>
+        <div class="info">
+        <ul>
+        <li> <h3>Likes</h3>
+          <p>${image.likes}</p></li>
+        <li> <h3>Views</h3>
+          <p>${image.views}</p></li>
+        <li> <h3>Comments</h3>
+          <p>${image.comments}</p></li>
+        <li> <h3>Downloads</h3>
+          <p>${image.downloads}</p></li>
+        </ul>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('visible');
+}
